9c9d8043e3ad4cee78bf39d7379fa1cac569b1f2
Denis Letor <denis.letor@external.total.com>
2019-05-23 17:01:18 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 # Create a resource group if it doesn’t exist
 resource "azurerm_resource_group" "rg" {
-  #count    = 0
+  count    = "${data.external.test_resourcegroup.result.RgExists == "true" ? 0 : 1 }"
+
   name     = "${var.client_rgName}"
   location = "${var.client_location}"
 
