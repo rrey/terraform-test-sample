93d802fce22c1dc8e4bdc25e433516eb77fe119a
Denis Letor <denis.letor@external.total.com>
2019-05-27 23:04:27 +0000

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,18 @@
 variable client_location {}
 variable client_rgName {}
-variable tagEnvironment {}
+#variable tagEnvironment {}
+
+variable "client_tags" {
+    description = "Tags will be used for Financial reporting and application lifecycle"
+    type = "map"
+    default = {
+        "Application name"      = ""
+        "Branch"                = ""
+        "Environment"           = ""
+        "Application Lifetime"  = ""
+        "Maintenance Window"    = ""
+        "Opening Hours"         = ""
+        "Exploitation"          = ""
+        "Security Level"        = ""
+    }
+}
