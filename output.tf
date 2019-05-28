4fe6e0a715bdb4ec96e851d8428dd1d19c0a7259
Denis Letor <denis.letor@external.total.com>
2019-05-28 12:48:21 +0000

    Updated output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "rgName" {
   description = "The resource group name"
-  value       = "${azurerm_resource_group.rg.name}"
+  value       = "${azurerm_resource_group.assie_rg.name}"
 }
 
 output "rgLocation" {
   description = "The resource group location"
-  value       = "${azurerm_resource_group.rg.location}"
+  value       = "${azurerm_resource_group.assie_rg.location}"
 }
 
 ## subscription Name
