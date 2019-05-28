073333eb7df3363dc3ef8c9a8605e8203129e1bf
Denis Letor <denis.letor@external.total.com>
2019-05-28 12:36:36 +0000

    subscription output var

--- output.tf
+++ output.tf
@@ -7,3 +7,13 @@ output "rgLocation" {
   description = "The resource group location"
   value       = "${azurerm_resource_group.rg.location}"
 }
+
+## subscription
+output "current_subscription_display_name" {
+  value = "${data.azurerm_subscription.current_subscription.display_name}"
+}
+
+output "current_subscription_location_id" {
+  value = "${data.azurerm_subscription.current_subscription.location_placement_id}"
+}
+location_placement_id
