3c52fa5c325a2e1eaf1e88dc19e1ddf0c6deccd8
Denis Letor <denis.letor@external.total.com>
2019-05-28 12:42:36 +0000

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -20,15 +20,27 @@ variable assie_tags {
 }
 
 variable assie_environmentCode {
-  type = "map"
-  default = {
-    production    = "p"
-    qualification = "q"
-    development   = "d"
-    pre-prod      = "r"
-    integration   = "i"
-    sandbox       = "s"
+    description = "Assie environment code digit"
+    type = "map"
+    default = {
+        production    = "p"
+        qualification = "q"
+        development   = "d"
+        pre-prod      = "r"
+        integration   = "i"
+        sandbox       = "s"
   }
+
+variable assie_subsciption_environment {
+    description = "Assie Subscription Name Environment"
+    type = "map"
+        prod = "production"
+        qual = "qualification"
+        dev  = "development"
+        hp   = "pre-prod"
+        int  = "integration"
+        sb   = "andbox"
+}
 
 variable env_environment {
     description = "value pass from the environment variable TF_VAR_env_environment"
