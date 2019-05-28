6b5f64cdaf2f0f0f3f9f259a664de6bdc4d96248
Denis Letor <denis.letor@external.total.com>
2019-05-28 00:09:27 +0000

    locals var

--- main.tf
+++ main.tf
@@ -1,11 +1,28 @@
-# Create a resource group if it doesn’t exist
-resource "azurerm_resource_group" "rg" {
+locals {
+  rgname = concat(aws_instance.blue.*.id, aws_instance.green.*.id)
+}
+
+locals {
+  local_tags = {
+    Service = local.service_name
+    Owner   = local.owner
+  }
+
+resource "azurerm_resource_group" "assie_rg" {
   count    = "${data.external.test_resourcegroup.result.RgExists == "true" ? 0 : 1 }"
 
   name     = "${var.client_rgName}"
   location = "${var.client_location}"
 
-  tags {
-    environment = "${var.tagEnvironment}"
+  tags = local.common_tags
+  
+#  tags = {
+#    environment = "${var.tagEnvironment}"
   }
 }
+
+tags {
+        Name = "${var.role} ${var_env}"
+        role = "${var.app_role}"
+        ${var.app_role} = "${var_env}"
+    }
