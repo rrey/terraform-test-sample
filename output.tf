b9e9140e59822c2944eb98d7d9229370c0a889ac
Denis Letor <denis.letor@external.total.com>
2019-05-29 13:41:08 +0000

    output "assieRgLocation"

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-output "rgName" {
+output "assieRgName" {
   description = "The resource group name"
   value       = "${azurerm_resource_group.assie_rg.name}"
 }
 
-output "rgLocation" {
+output "assieRgLocation" {
   description = "The resource group location"
   value       = "${azurerm_resource_group.assie_rg.location}"
 }
