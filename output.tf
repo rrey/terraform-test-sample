581f639595c5d78903ad02ea939b338969af089b
Denis Letor <denis.letor@external.total.com>
2019-05-29 14:25:31 +0000

    Updated output.tf

--- output.tf
+++ output.tf
@@ -8,9 +8,19 @@ output "assieRgLocation" {
   value       = "${azurerm_resource_group.assie_rg.location}"
 }
 
-output "tagApplicationName" {}
+output "tagApplicationName" {
+  description =
+  
+}
 output "tagBranch" {}
 output "tagEnvironment" {}
 output "tagApplicationLifetime" {}
 output "tagExploitation" {}
 output "tagSecurityLevel" {}
+
+    ApplicationName     = local.l_tag_application_name
+    Branch              = local.l_tag_branch_code
+    Environment         = local.l_tag_environment
+    ApplicationLifetime = local.l_tag_application_lifetime
+    Exploitation        = local.l_tag_exploitation
+    SecurityLevel       = local.l_tag_security_level_code
