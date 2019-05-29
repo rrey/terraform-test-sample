0888e1d8830d392c9033b1fee5586f862d8df838
Denis Letor <denis.letor@external.total.com>
2019-05-29 09:29:58 +0000

    ### map for Total SecurityLevel   l_security_level

--- main.tf
+++ main.tf
@@ -1,9 +1,41 @@
 locals {
-  l_total_cmdb_code    = lower(substr("TEST",1,4))
+  ### Cmdb code to determine - TEST for testing
+  l_cmdb_application_code    = lower(substr("TEST",1,4))
+
+  ### map for environment code
+  local l_environment_code {
+    production    = "p"
+    qualification = "q"
+    development   = "d"
+    pre-prod      = "r"
+    integration   = "i"
+    sandbox       = "s"
+    }
+
+  ### map for Total Branch
+  l_branch {
+    ms = "MS" 
+    ep = "EP"
+    rc = "RC" 
+    gp = "GP" 
+    hd = "HD" 
+    ts = "TS"
+  }
+
+  ### map for Total SecurityLevel
+  l_security_level {
+    
+  }
+
+  ### variable environment from pipepline TF_VAR_environment external variable
   l_environment      = lower("${var.assie_environment}")
+
+  ### calculate environment variable
   l_environment_code = "${var.assie_environmentCode["${local.l_environment}"]}"
-  l_tag = {
-    ApplicationName      = concat("${local.l_total_cmdb_code},"-","${local.l_environment})
+
+  ### tags for resource groupe base on HLD
+  l_assie_tag = {
+    ApplicationName      = concat("${local.l_cmdb_application_code},"-","${local.l_environment_code})
     Branch               = ""
     Environment          = local.l_environment
     ApplicationLifetime  = ""
@@ -12,23 +44,13 @@ locals {
     Exploitation         = ""
     SecurityLevel        = ""
   }
-  l_assie_rgname  = concat("az", "rg", var.assie_environmentCode["${local.l_environment}"], local.l_totalCmdbCode, "01")
+  ### Calculate Resource group name base on HLD
+  l_assie_rgname  = concat("az", "rg", "${local.l_environment_code}", "${local.l_cmdb_application_code}, "01")
 }
 
-locals {
-
-}
-
-
-
+### Generate Resource Group
 resource "azurerm_resource_group" "assie_rg" {
-
   name     = "${local.l_assie_rgname}"
   location = "${var.module_location}"
-
-  tags     = "${local.l_tags}"
-  
-#  tags = {
-#    ApplicationName = "${data.rg.tagApplication}"
-  }
+  tags     = "${local.l_assie_tags}"
 }
