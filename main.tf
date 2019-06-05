c6f1ee45f3249fb23c24f6ae7907744cf87a5eef
Denis Letor <denis.letor@external.total.com>
2019-06-05 14:05:23 +0000

    locals

--- main.tf
+++ main.tf
@@ -15,9 +15,7 @@ locals {
     int = "i"
     san = "s"
     }
-}
 
-locals {
   ### map for Total Branch
   l_branch_map = {
     ms = "MS" 
@@ -27,82 +25,56 @@ locals {
     hd = "HD" 
     ts = "TS"
   }
-}
 
-locals {
   ### map for Total SecurityLevel
   l_security_level_map = {
     standard    = "Standard"
     high        = "High"
     conditional = "Conditional"
   }
-}
 
-locals {
   ### variable applicationName from TF_VAR_assie_applicationName
   l_application_name = trimspace(lower("${var.assie_applicationName}"))
 
   ### variable application code from CMDB
   l_cmdb_application_code = lower(substr(local.l_application_name,0,4))
-}
 
-locals {
   ### variable environment from  TF_VAR_assie_environment
   l_tag_environment = lower(substr("${var.assie_environment}",0,3))
-}
 
-locals {
   ### variable security level from TF_VAR_assie_securityLevel
   l_securitylevelvar = lower("${var.assie_securityLevel}")
-}
 
-locals {
   ### Calculate tag exploitation from TF_VAR_assie_exploitation
   l_exploitation = lower("${var.assie_exploitation}") 
-}
 
-locals {
   ### Calculate tag exploitation from TF_VAR_assie_branch
   l_branchvar = lower(substr("${var.assie_branch}",0,2))
-}
 
-locals {
   ### Calculate tag Branch code
   l_tag_branch = local.l_branch_map[local.l_branchvar]
-}
 
-locals {
   ### Calculate tag Application Name
   ## Calculate environment Code
   l_environment_code = local.l_environment_map[local.l_tag_environment]
-}
-locals {
+
   # Calculate tag Application Name
   l_tag_application_name = "${local.l_application_name}-${local.l_environment_code}"
-}
 
-locals {
   ### Calulate tag Application Life Time (end date) - test 1 year
   ## timeadd(time, duration)
   ## Returns a UTC timestamp string corresponding to adding a given duration to time in RFC 3339 format.
   l_application_end_date = "1y"
-}
-locals {
+
   # Calculate tag Application LifeTime
   l_tag_application_lifetime = timeadd(timestamp(), local.l_application_end_date)
-}
 
-locals {
   ### Calculate tag exploitation from TF_VAR_assie_exploitation
   l_tag_exploitation = upper("${var.assie_exploitation}") 
-}
 
-locals {
   ### Calculate tag Security Level
   l_tag_security_level = local.l_security_level_map[local.l_securitylevelvar]
-}
 
-locals {
   ### tags for resource groupe base on HLD on SandBox
   l_assie_tag_sandbox = {
     ApplicationName     = local.l_tag_application_name
@@ -121,10 +93,8 @@ locals {
     Exploitation        = local.l_tag_exploitation
     SecurityLevel       = local.l_tag_security_level
   }
-}
 
-### Calculate Resource Group Name base on HLD
-locals {
+  ### Calculate Resource Group Name base on HLD
   l_cloud_code     = "az"
   l_resource_code  = "rg"
   l_resource_index = "01"
