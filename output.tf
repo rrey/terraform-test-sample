d58e38b823dc03eb097465d487a48883f641034f
Denis Letor <denis.letor@external.total.com>
2019-06-07 12:10:55 +0000

    Updated output.tf

--- output.tf
+++ output.tf
@@ -1,13 +1,11 @@
 output "assieRgName" {
   description = "The resource group name"
-#  value       = "${azurerm_resource_group.assie_rg[0].name}"
-  value       = "${azurerm_resource_group.assie_rg.name}"
+  value       = "${azurerm_resource_group.assie_rg[0].name}"
 }
 
 output "assieRgLocation" {
   description = "The resource group location"
-#  value       = "${azurerm_resource_group.assie_rg[0].location}"
-  value       = "${azurerm_resource_group.assie_rg.location}"
+  value       = "${azurerm_resource_group.assie_rg[0].location}"
 }
 
 output "tagApplicationName" {
