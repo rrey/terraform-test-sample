e212771c67623a6a448d033391db0fa6d1ac4f6c
Clement Guiheneuf <clementg@exakis.com>
2019-06-12 07:54:24 +0000

    Updated output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-output "assieRgName" {
+output "outputRgName" {
   description = "The resource group name"
   value       = "${azurerm_resource_group.assie_resource_group[0].name}"
 }
 
-output "assieRgLocation" {
+output "outputRgLocation" {
   description = "The resource group location"
   value       = "${azurerm_resource_group.assie_resource_group[0].location}"
 }
