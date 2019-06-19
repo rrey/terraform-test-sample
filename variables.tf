2f4c9a50080cebb66e0e9e94ae55e901b6df2415
Denis Letor <denis.letor@external.total.com>
2019-06-19 16:21:13 +0000

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -4,6 +4,11 @@ variable "assie_location" {
     default     = "westeurope"
 }
 
+variable "assie_districtCode" { 
+    description = "Code of District - from a variable or the environment variable TF_VAR_assie_districtCode"
+    default = ""
+}
+
 variable "assie_applicationName" {
     description = "Name of application - from a variable or the environment variable TF_VAR_assie_applicationName"
     type        = string
