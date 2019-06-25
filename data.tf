ab7899608d8ac8c8b342452b2c704df0beef858d
Denis Letor <denis.letor@external.total.com>
2019-06-25 09:15:55 +0000

    Updated data.tf

--- data.tf
+++
@@ -1,4 +0,0 @@
-## Test if Client Resource Group exist
-#data "external" "test_resourcegroup" {
-#  program  = ["bash", "-c", "if [ -z \"$(az group exists -n \"${local.l_rgname}\")\" ]; then echo '{\"RgExists\": \"true\"}'; else echo '{\"RgExists\": \"false\"}'; fi"]
-#}
