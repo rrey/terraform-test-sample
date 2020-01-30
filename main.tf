34201f629d475eb88fb83c912a75a6237abfaa95
Denis Letor <denis.letor@external.total.com>
2020-01-30 11:38:25 +0000

    assie back

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ locals {
 }
 
 ### Generate Resource Group
-resource "azurerm_resource_group" "arcade_resource_group" {
+resource "azurerm_resource_group" "assie_resource_group" {
   count    = tonumber("${local.l_tag_environment == false ? 0 : 1 }") * tonumber("${var.module_create == true ? 1 : 0}")
   location = "${var.assie_location}"
   name     = local.l_rgname
